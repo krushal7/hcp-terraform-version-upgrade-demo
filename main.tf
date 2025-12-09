4b0e3fea1ffb8b06e2bbfe793f82942548581c22
krushal7 <94886146+krushal7@users.noreply.github.com>
2025-12-09 18:12:37 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "~> 1.10.0"
+
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.7"
+    }
+  }
+}
+
+provider "random" {}
+
+resource "random_pet" "example" {
+  length = 2
+}
+
+output "pet_name" {
+  value = random_pet.example.id
+}
